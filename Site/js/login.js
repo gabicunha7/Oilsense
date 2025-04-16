d96e330e6f3da0d8b949d861d5a6535d73e7e9e0
gabicunha7 <gabriela.cunha@sptech.school>
2025-04-16 20:36:27 -0300

    altera laço repetição

--- Site/js/login.js
+++ Site/js/login.js
@@ -1,29 +1,46 @@
 function entrar() {
     let emailDigitado = ipt_email.value;
     let senhaDigitada = ipt_senha.value;
+    let senhaIncorreta = false;
 
     let listaEmail = [
-        'bmw@gmail.com',
-        'volkswagen@gmail.com',
-        'ford@gmail.com',
-        'pegeout@gmail.com',
-        'nissan@gmail.com',
-        'mazba@gmail.com',
-        'lamborghini@gmail.com'
+        'toyota@gmail.com',
+        'hyundai@gmail.com',
+        'honda@gmail.com',
+        'lucas.silva@toyota.com',
+        'mariana.costa@toyota.com',
+        'carlos.souza@hyundai.com',
+        'fernanda.oliveira@hyundai.com',
+        'andre.pereira@honda.com',
+        'beatriz.martins@honda.com',
+        'joao.lima@honda.com'
     ];
 
     let listaSenha = [
-        'Bmw12345',
-        'VolksWagen12345',
-        'Ford12345',
-        'Pegeout12345',
-        'Mazba12345',
-        'Lamborghini12345'
+        'e4484b99e445cc97ac2acafbd965579954989dd0b64bf4c012d8727f230388af',
+        '0b9fea9badc53a9775d7f11cace2f8edb29f270d9eda7c8d56b661b1e9a35ccd',
+        '463a83bc0ca1971faeffc7938cc80c040a8049ce48c6c251ac7124a715c8278e',
+        'senha1',
+        'senha2',
+        'senha3',
+        'senha4',
+        'senha5',
+        'senha6',
+        'senha7'
     ];
 
     for (let i = 0; i < listaEmail.length; i++) {
         if (emailDigitado == listaEmail[i] && senhaDigitada == listaSenha[i]) {
+            senhaIncorreta = false;
             window.location.href = 'dashboards.html';
+            break;
+        } else {
+            senhaIncorreta = true;
         }
     }
+    if (senhaIncorreta){
+        alert('Senha ou Email incorretos.');
+    }
+
+    
 }
