07dc4cb73c66cd51027120d7fea3717f6cdb7892
gabicunha7 <gabriela.cunha@sptech.school>
2025-05-10 17:52:57 -0300

    mais alterações api

--- Site-V2/src/controllers/carroController.js
+++ Site-V2/src/controllers/carroController.js
@@ -1,60 +1,28 @@
 var carroModel = require("../models/carroModel");
 
-function autenticarModelo(req, res) {
-    var modelo = req.body.modeloServer;
-
-    if (modelo == undefined) {
-	res.status(400).send("Seu modelo está indefinido!");
-    } 
-    else {
-
-	carroModel.autenticarModelo(modelo)
-	    .then(
-		function (resultadoAutenticar) {
-		    console.log(`\nResultados encontrados: ${resultadoAutenticar.length}`);
-		    console.log(`Resultados: ${JSON.stringify(resultadoAutenticar)}`); // transforma JSON em String
-
-		    if (resultadoAutenticar.length == 1) {
-			console.log(resultadoAutenticar);	
-				    res.json({
-					modelo: resultadoAutenticar[0].modelo
-				    });
-		    } else  {
-			res.status(403).send("Modelo do carro inválido");
-		    }
-		}
-	    ).catch(
-		function (erro) {
-		    console.log(erro);
-		    console.log("\nHouve um erro ao realizar o login! Erro: ", erro.sqlMessage);
-		    res.status(500).json(erro.sqlMessage);
-		}
-	    );
-    }
-}
-
-
 function cadastrarCarro(req, res) {
-    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+
     var placa = req.body.placaServer;
     var volume = req.body.volumeServer;
     var altura = req.body.alturaServer;
     var modelo = req.body.modeloServer;
+    var ano = req.body.anoServer;
 
-    // Faça as validações dos valores
+
     if (placa == undefined) {
-	res.status(400).send("Seu placa está undefined!");
+	res.status(400).send("Sua placa está undefined!");
     } else if (volume == undefined) {
 	res.status(400).send("Seu volume está undefined!");
     } else if (altura == undefined) {
-	res.status(400).send("Seu altura está undefined!");
+	res.status(400).send("Sua altura está undefined!");
     } else if (modelo == undefined) {
-	res.status(400).send("Sua modelo está undefined!");
+	res.status(400).send("Seu modelo está undefined!");
+    }  else if (ano == undefined) {
+	res.status(400).send("Seu ano está undefined!");
     }  
     else {
 
-	// Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-	carroModel.cadastrarCarro(placa, volume, altura, modelo)
+	carroModel.cadastrarCarro(placa, volume, altura, modelo, ano)
 	    .then(
 		function (resultado) {
 		    res.json(resultado);
@@ -73,6 +41,5 @@ function cadastrarCarro(req, res) {
 }
 
 module.exports = {
-    autenticarModelo,
     cadastrarCarro
 }
--- Site-V2/src/models/carroModel.js
+++ Site-V2/src/models/carroModel.js
@@ -1,29 +1,22 @@
 var database = require("../database/config")
 
-function autenticarModelo(modelo) {
-	console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", modelo)
-	var instrucaoSql = `
-	SELECT fkmodelo FROM modelo WHERE nome = '${modelo}';
-    `;
-	console.log("Executando a instrução SQL: \n" + instrucaoSql);
-	return database.executar(instrucaoSql);
-}
-
 function cadastrarCarro(placa, volume, altura, modelo, ano) {
 	console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarCarro():", placa, volumecarter, alturacarter, fkmodelo);
 
 	// Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
 	//  e na ordem de inserção dos dados.
 	var instrucaoSql = `
-        INSERT INTO carro (placa, volumecarter, alturacarter, fkmodelo) VALUES ('${placa}', ${volume}, ${altura}, (select fkmodelo
-from modelo where modelo = '${modelo}' and ano = '${ano}'));
-    `;
+	set @idsensor = null;
+	insert into sensor(status) values ('Ativo'); 
+	set @idsensor = last_insert_id();
+        INSERT INTO carro (placa, volumecarter, alturacarter, fkmodelo, fksensor) VALUES ('${placa}', ${volume}, ${altura}, (select fkmodelo
+from modelo where modelo = '${modelo}' and ano = ${ano}), @idsensor);
+ `; // perguntar se posso fazer com variaveis do sql se não tem que separar em partes, criação de sensor e inserção
 	console.log("Executando a instrução SQL: \n" + instrucaoSql);
 	return database.executar(instrucaoSql);
 }
 
 
 module.exports = {
-	cadastrarCarro,
-	autenticarModelo
+	cadastrarCarro
 };
