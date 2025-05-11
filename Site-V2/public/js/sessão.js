f6670d772eb8135568f5d23e21c7bbbb8906c90f
jpPinheiro03 <joao.pinheiro@sptech.school>
2025-05-11 19:47:12 -0300

    75% do login

---
+++ Site-V2/public/js/sessão.js
@@ -0,0 +1,34 @@
+function validarSessao() {
+    var email = sessionStorage.EMAIL_USUARIO;
+    var senha = sessionStorage.SENHA_USUARIO;
+
+    var b_usuario = document.getElementById("b_usuario");
+
+    if (email != null && senha != null) {
+        b_usuario.innerHTML = senha;
+    } else {
+        window.location = "../login.html";
+    }
+}
+
+function limparSessao() {
+    sessionStorage.clear();
+    window.location = "../login.html";
+}
+
+// carregamento (loading)
+function aguardar() {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "flex";
+}
+
+function finalizarAguardar(texto) {
+    var divAguardar = document.getElementById("div_aguardar");
+    divAguardar.style.display = "none";
+
+    var divErrosLogin = document.getElementById("div_erros_login");
+    if (texto) {
+        divErrosLogin.style.display = "flex";
+        divErrosLogin.innerHTML = texto;
+    }
+}
