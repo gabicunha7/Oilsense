fe003c2e9ba1dbbb3ab6cb88dc186cdb967d268d
gabicunha7 <gabriela.cunha@sptech.school>
2025-06-02 22:26:14 -0300

    alguns selects para os gráficos de barra e linha

--- Site/src/models/graficosModel.js
+++ Site/src/models/graficosModel.js
@@ -1,5 +1,50 @@
 var database = require("../database/config");
 
+
+
+function porcentagemCarroPorPlaca(placa) {
+    console.log("ACESSEI O FUNCIONARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarFuncionarios()");
+
+    var instrucaoSql = `
+        select round(((c.alturacarter - t.distancia) / c.alturacarter) * 100, 1) porcentagem,
+        m.modelo, date_format(t.dtHoraColeta,'%d/%m') dia_mes
+        from carro c
+        inner join sensor s   
+                on c.fksensor = s.id
+        inner join telemetria t
+                on t.fksensor = s.id
+        inner join modelo m
+                on c.fkmodelo = m.id
+        where placa = '${placa}' limit 7;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function porcentagemMediaModelo(modelo_id) {
+    console.log("ACESSEI O FUNCIONARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarFuncionarios()");
+
+    var instrucaoSql = `
+        select round(avg(((c.alturacarter - t.distancia) / c.alturacarter) * 100), 1) porcentagem,
+        m.modelo, date_format(t.dtHoraColeta,'%d/%m') dia_mes
+        from carro c
+        inner join sensor s   
+                on c.fksensor = s.id
+        inner join telemetria t
+                on t.fksensor = s.id
+        inner join modelo m
+                on c.fkmodelo = m.id
+        where m.id = ${modelo_id}
+        group by m.modelo, dia_mes limit 7;
+    `;
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    graficosModel
+    graficosModel,
+    porcentagemCarroPorPlaca,
+    porcentagemMediaModelo
 }
