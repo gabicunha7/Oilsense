5065f57e7294f9f71d1f16b6647d504667efe4ca
ThaisKheyla <kheyla.paucara@sptech.school>
2025-06-10 12:59:17 -0300

    modelos listar

--- Site/src/models/graficosModel.js
+++ Site/src/models/graficosModel.js
@@ -23,9 +23,24 @@ function carrosAlerta(alerta, montadora) {
         return database.executar(instrucaoSql);
 }
 
+function modelosAlerta(alerta, montadora) {
+        console.log("ACESSEI O GRAFICO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function nivelDeAlertaPorMes()");
+
+        var instrucaoSql = `
+                select count(nivel_oleo) qtd, nivel_oleo, modelo from vw_listar_alertas
+                where id = ${montadora} and nivel_oleo = ${alerta}
+                group by nivel_oleo, modelo;
+        `;
+
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+}
+
+
 
 
 module.exports = {
         nivelDeAlertaPorDia,
-        carrosAlerta
+        carrosAlerta,
+        modelosAlerta
 }
