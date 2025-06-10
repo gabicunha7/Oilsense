2b47d3505f8384ba602365c4722234c59cd4d7a3
httpsrenan <renan.pereira@sptech.school>
2025-06-10 12:35:51 -0300

    Adicionando função de carros

--- Site/public/js/graficos.js
+++ Site/public/js/graficos.js
@@ -26,6 +26,42 @@ function alertasGraficoDePizza() {
     return false;
 }
 
+function veiculosComAlerta(alerta) {
+    let montadora_id = sessionStorage.ID_MONTADORA;
+
+    fetch(`/graficos/carrosAlerta`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            alerta: alerta,
+            montadora: montadora_id
+        }),
+    })
+        .then(function (resposta) {
+            if (resposta.ok) {
+                if (resposta.statusText == 'No Content') {
+                    let secGrafico = document.querySelector('#carros .grafico');
+                    let mensagem = document.querySelector('#carros .mensagem');
+
+                    secGrafico.style.display = 'none';
+                    mensagem.innerHTML = `Nenhum carro com ${alerta}`;
+                } else {
+                    resposta.json().then(function (dados) {
+                        console.log("graficos:", dados);
+                    });
+                }
+            } else {
+                alert("Houve um erro ao tentar puxar os dados!");
+            }
+        })
+        .catch(function (erro) {
+            console.error("#ERRO: ", erro);
+            alert("Erro ao comunicar com o servidor.");
+        });
+}
+
 function plotarGraficoPizza(dados) {
     let labels = [];
 
@@ -114,3 +150,37 @@ function exibir(tipo) {
         }
     }
 }
+
+function exibirNivel(tipo, tipoGrafico) {
+    let botoesTipos = document.querySelectorAll(`#${tipoGrafico} .botoes-tipos-alertas  button`);
+    let mensagem = document.querySelector(`#${tipoGrafico} .mensagem`);
+    let secGrafico = document.querySelector('#carros .grafico');
+
+    secGrafico.style.display = 'block';
+    mensagem.innerHTML = '';
+
+    if (tipoGrafico == 'carros') {
+        veiculosComAlerta(tipo);
+    } else {
+    }
+
+    let indice = 0;
+    if (tipo == 'nivel1') {
+        indice = 0;
+    } else if (tipo == 'nivel2') {
+        indice = 1;
+    } else {
+        indice = 2;
+    }
+
+    if (!botoesTipos[indice].classList.contains('btn-selecionado')) {
+        botoesTipos[indice].classList.add('btn-selecionado');
+    }
+
+    for (let i = 0; i < botoesTipos.length; i++) {
+        if (botoesTipos[i].classList.contains('btn-selecionado') && botoesTipos[i] != botoesTipos[indice]) {
+            botoesTipos[i].classList.toggle('btn-selecionado');
+        }
+    }
+}
+
