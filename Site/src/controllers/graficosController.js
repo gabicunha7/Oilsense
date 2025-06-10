e5815026797d5ce388a72a505f3efb31cc534136
ThaisKheyla <kheyla.paucara@sptech.school>
2025-06-10 12:12:47 -0300

    controller listar carros alertas

--- Site/src/controllers/graficosController.js
+++ Site/src/controllers/graficosController.js
@@ -26,7 +26,34 @@ function nivelDeAlertaPorDia(req, res) {
         );
 }
 
+function carrosAlerta(req, res) {
+    var alerta = req.body.alerta;
+    var montadora = req.body.montadora;
+
+    // Validações individuais
+    if (montadora == undefined) {
+        res.status(400).send("O montadora está undefined!");
+    } else if (alerta == undefined) {
+        res.status(400).send("O alerta está undefined!");
+    } else {
+        graficosModel.carrosAlerta(alerta, montadora)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            )
+            .catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log("\nHouve um erro ao listar alertas dos carros! Erro: ", erro.sqlMessage);
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
     nivelDeAlertaPorDia,
+    carrosAlerta
 
 }
