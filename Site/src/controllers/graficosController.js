015ceb0a0145cbd182cf863f466e39727c12eb47
ThaisKheyla <kheyla.paucara@sptech.school>
2025-06-10 13:32:24 -0300

    controller

--- Site/src/controllers/graficosController.js
+++ Site/src/controllers/graficosController.js
@@ -30,7 +30,6 @@ function carrosAlerta(req, res) {
     var alerta = req.body.alerta;
     var montadora = req.body.montadora;
 
-    // Validações individuais
     if (montadora == undefined) {
         res.status(400).send("O montadora está undefined!");
     } else if (alerta == undefined) {
@@ -60,7 +59,6 @@ function modelosAlerta(req, res) {
     var alerta = req.body.alerta;
     var montadora = req.body.montadora;
 
-    // Validações individuais
     if (montadora == undefined) {
         res.status(400).send("O montadora está undefined!");
     } else if (alerta == undefined) {
@@ -86,9 +84,35 @@ function modelosAlerta(req, res) {
     }
 }
 
+function graficoPorCarro(req, res) {
+    var codigo = req.params.codigo;
+
+    graficosModel.graficoPorCarro(codigo)
+        .then(
+            function (resultado) {
+                if (resultado.length > 0) {
+                    res.status(200).json(resultado);
+                } else {
+                    res.status(204).send("Nenhum resultado encontrado!");
+                }
+            }
+        )
+        .catch(
+            function (erro) {
+                console.log(erro);
+                console.log(
+                    "Houve um erro ao buscar os alertas: ",
+                    erro.sqlMessage
+                );
+                res.status(500).json(erro.sqlMessage);
+            }
+        );
+}
+
 module.exports = {
     nivelDeAlertaPorDia,
     carrosAlerta,
-    modelosAlerta
+    modelosAlerta,
+    graficoPorCarro
 
 }
